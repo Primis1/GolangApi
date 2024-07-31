46c5bed6aebad768383b36b8483383de7f2c455c
Olek <149747215+Primis1@users.noreply.github.com>
2024-07-30 22:03:30 -0400

    fix config setup

--- cmd/internal/config/config.go
+++ cmd/internal/config/config.go
@@ -4,37 +4,41 @@ import (
 	"log"
 	"os"
 	"time"
-
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+
 type Config struct {
-	Env          string `yaml:"env" env:"ENV" envDefault:"local"`
-	Storage_path string `yaml:"storage_path" env-required:"true"`
-	HTTPServer   `yaml:"http_server"`
+    Env         string `yaml:"env" env-default:"development"`
+    StoragePath string `yaml:"storage_path" env-required:"true"`
+    HTTPServer `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address      string        `yaml:"address" env-dafault:"localhost:8888"`
-	Time         time.Duration `yaml:"timeout" env_default:"4s"`
-	Idle_timeout time.Duration `yaml:"idle_timeout" env_default:"60s"`
+    Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+    Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+    IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
-	configPath := os.Getenv("CONFIG_PATH")
-	if configPath == "" {
-		log.Fatalf("config file %s\n", configPath)
-	}
+    // Получаем путь до конфиг-файла из env-переменной CONFIG_PATH
+    configPath := os.Getenv("CONFIG_PATH")
+    if configPath == "" {
+        log.Fatal("CONFIG_PATH environment variable is not set")
+    }
 
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exist %s\n", configPath)
-	}
+    // Проверяем существование конфиг-файла
+    if _, err := os.Stat(configPath); err != nil {
+        log.Fatalf("error opening config file: %s", err)
+    }
 
-	var cfg Config
+    var cfg Config
 
-	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
-		log.Fatalf("cannot read config: %s\n", err)
-	}
+    // Читаем конфиг-файл и заполняем нашу структуру
+    err := cleanenv.ReadConfig(configPath, &cfg)
+    if err != nil {
+        log.Fatalf("error reading config file: %s", err)
+    }
 
-	return &cfg
+    return &cfg
 }
--- cmd/url-shorter/main.go
+++ cmd/url-shorter/main.go
@@ -8,30 +8,31 @@ import (
 
 const (
 	envLocal = "local"
-	envDev = "dev"
-	envProd = "prod" 
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	cfg := config.MustLoad()
-	
-	log := Logger(cfg.Env)
-	log = log.With(slog.String("env", cfg.Env))
+    cfg := config.MustLoad()
 
-	log.Info("initialize server", slog.String("address", cfg.Address))
-	log.Debug("logger debug mode enabled")
+    log := setupLogger(cfg.Env)
+    log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+    log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+    log.Debug("logger debug mode enabled")
 }
 
-func Logger(env string) *slog.Logger {
-	var log *slog.Logger 
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
 
-	switch env{
+    switch env {
     case envLocal:
         log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
     case envDev:
         log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
     case envProd:
         log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-	}
-	return log
+    }
+
+    return log
 }
