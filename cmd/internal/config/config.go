c1480bab9a5787c6b210bb1bcad0b77ca0441ff4
Olek <149747215+Primis1@users.noreply.github.com>
2024-08-13 04:16:35 -0400

    add base routing

--- cmd/internal/config/config.go
+++ cmd/internal/config/config.go
@@ -21,8 +21,7 @@ type HTTPServer struct {
 }
 
 func MustLoad() *Config {
-    // Получаем путь до конфиг-файла из env-переменной CONFIG_PATH
-    configPath := os.Getenv("CONFIG_PATH")
+    configPath := os.Getenv(CONFIG_PATH)
     if configPath == "" {
         log.Fatal("CONFIG_PATH environment variable is not set")
     }
--- cmd/url-shorter/main.go
+++ cmd/url-shorter/main.go
@@ -1,38 +1,69 @@
+// package main
+
+// import (
+// 	"log/slog"
+// 	"os"
+// 	"url-shortener/internal/config"
+// )
+
+// const (
+// 	envLocal = "local"
+// 	envDev   = "dev"
+// 	envProd  = "prod"
+// )
+
+// func main() {
+//     cfg := config.MustLoad()
+
+//     log := setupLogger(cfg.Env)
+//     log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+//     log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+//     log.Debug("logger debug mode enabled")
+// }
+
+// func setupLogger(env string) *slog.Logger {
+//     var log *slog.Logger
+
+//     switch env {
+//     case envLocal:
+//         log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+//     case envDev:
+//         log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+//     case envProd:
+//         log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+//     }
+
+//     return log
+// }
+
 package main
 
 import (
-	"log/slog"
-	"os"
-	"url-shortener/internal/config"
-)
+	"log"
+	"net/http"
+) 
 
-const (
-	envLocal = "local"
-	envDev   = "dev"
-	envProd  = "prod"
-)
-
-func main() {
-    cfg := config.MustLoad()
-
-    log := setupLogger(cfg.Env)
-    log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
-
-    log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
-    log.Debug("logger debug mode enabled")
+func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+        http.NotFound(w, r)
+        return 
+    }
+    w.Write([]byte("Hello my server"))
 }
 
-func setupLogger(env string) *slog.Logger {
-    var log *slog.Logger
+func snippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Snippet"))
+}
 
-    switch env {
-    case envLocal:
-        log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-    case envDev:
-        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
-    case envProd:
-        log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
-    }
+func main() {
+    mux := http.NewServeMux()
 
-    return log
+    mux.HandleFunc("/", home)
+    mux.HandleFunc("/snip", snippet)
+
+    log.Print("Starting server on :4000")
+
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
