8fd0be3a46a038d11be0f5615e45089786fa20c9
root <root@DESKTOP-7FFGMOG>
2024-09-04 00:05:01 -0400

    add logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,46 @@
-
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
-
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer f.Close()
+
+	infoLog := log.New(f, "INFO: \t", log.Ldate|log.Ltime)
+	// errorLog := log.New(os.Stderr, "ERROR: \t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
+
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Register the other application routes as normal.
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+
+	infoLog.Printf("Starting server on %s", *addr)
+
+	// srv := &http.Server{
+	// 	Addr:     *addr,
+	// 	ErrorLog: errorLog,
+	// 	Handler:  mux,
+	// }
+
+	// err := srv.ListenAndServe()
+	// errorLog.Fatal(err)
 }
